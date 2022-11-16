9ec6835ca1f7441a4089a07e0623e44111ab1236
P3rr000 <javier.bxx@gmail.com>
2022-11-16 03:17:52 -0300

    lol1

--- app/src/main/java/com/example/infomanga2_0/MainActivity.java
+++ app/src/main/java/com/example/infomanga2_0/MainActivity.java
@@ -3,12 +3,38 @@ package com.example.infomanga2_0;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText username, password, repassword;
+    Button singup, singin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        username = (EditText) findViewById(R.id.username);
+        password = (EditText) findViewById(R.id.password);
+        repassword = (EditText) findViewById(R.id.repassword);
+        singup = (Button) findViewById(R.id.btnsingup);
+        singin = (Button) findViewById(R.id.btnsingin);
+
+        singup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+        singin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
     }
 }
