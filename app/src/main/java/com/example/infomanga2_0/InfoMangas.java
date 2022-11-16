fa207ff328584fc125b5e5cf7facd38f94cea2d7
P3rr000 <javier.bxx@gmail.com>
2022-11-16 14:41:17 -0300

    lol2

--- app/src/main/java/com/example/infomanga2_0/InfoMangas.java
+++ app/src/main/java/com/example/infomanga2_0/InfoMangas.java
@@ -2,16 +2,63 @@ package com.example.infomanga2_0;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.View;
 
 public class InfoMangas extends AppCompatActivity {
 
+    SensorManager sensorManager;
+    Sensor sensor;
+    SensorEventListener sensorEventListener;
+
+    @Override
+    protected void onPause() {
+        stop();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+        start();
+        super.onResume();
+    }
+
+    @SuppressLint("ServiceCast")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_info_mangas);
+
+        sensorManager=(SensorManager) getSystemService(SENSOR_SERVICE);
+        sensor=sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        if (sensor==null)
+            finish();
+
+        sensorEventListener=new SensorEventListener() {
+            @Override
+            public void onSensorChanged(SensorEvent sensorEvent) {
+                if(sensorEvent.values[0]<sensor.getMaximumRange()){
+                    getWindow().getDecorView().setBackgroundColor(Color.RED);
+                }
+                else{
+                    getWindow().getDecorView().setBackgroundColor(Color.DKGRAY);
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int i) {
+
+            }
+        };
+        start();
     }
     public void MostrarLista(View view) {
         Intent mostrarLista = new Intent(this, CardView.class);
@@ -21,5 +68,12 @@ public class InfoMangas extends AppCompatActivity {
     public void Volver(View view) {
         Intent volver = new Intent(this, HomeActivity.class);
         startActivity(volver);
+    }
+
+    public void start(){
+        sensorManager.registerListener(sensorEventListener,sensor, 2000*1000);
+    }
+    public void stop(){
+        sensorManager.unregisterListener(sensorEventListener);
     }
 }
