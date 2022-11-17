8d69c02c3fe3169eb99fd0e385d9acdbbad6cff5
P3rr000 <javier.bxx@gmail.com>
2022-11-17 03:41:33 -0300

    lol7

--- app/src/main/java/com/example/infomanga2_0/HomeActivity.java
+++ app/src/main/java/com/example/infomanga2_0/HomeActivity.java
@@ -7,7 +7,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-public class HomeActivity extends Activity {
+public class HomeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
